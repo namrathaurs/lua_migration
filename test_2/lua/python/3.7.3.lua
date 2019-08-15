74590792892a8759981386dac33074e29c79d3ce
namrathaurs@my.unt.edu <nvu0001@t3-login1.cm.cluster>
2019-08-15 16:57:31 -0500

    uncommented the invocation of lua script

--- test_2/lua/python/3.7.3.lua
+++ test_2/lua/python/3.7.3.lua
@@ -5,5 +5,5 @@ prepend_path{"PATH","/cm/shared/utils/PYTHON/3.7.3/bin",delim=":",priority="0"}
 prepend_path{"LD_LIBRARY_PATH","/cm/shared/utils/PYTHON/3.7.3/lib",delim=":",priority="0"}
 
 local usr_msg_file = pathJoin(dirname(myFileName()),".user_message.lua")
-LmodMessage(usr_msg_file)
---loadfile(usr_msg_file)()
+--LmodMessage(usr_msg_file)
+loadfile(usr_msg_file)()
